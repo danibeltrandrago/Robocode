9ca8dde3c88fc9e1aa7a60f0770a3bae8e041060
danibeltran <danibeltrandrago@gmail.com>
2019-10-11 10:12:32 +0200

    Repo1

---
+++ src/myfirstrobot/MyFirstRobot.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myfirstrobot;
+import robocode.*;
+
+/**
+ *
+ * @author dani
+ */
+public class MyFirstRobot extends Robot{
+
+    public void run(){
+    
+    }
+    
+}
